96804d0b74ba79fcf68e85ba8a0ebc76abc388b8
Jakecw97 <g00349377@gmit.ie>
2019-04-16 11:56:46 +0100

    Restart
    First Commit

---
+++ LP/LP/LP/Services/Storage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LP.Services
+{
+    public interface Storag<T>
+    {
+        Task<bool> AddItemAsync(T Workout);
+        Task<bool> UpdateItemAsync(T Workout);
+        Task<bool> DeleteItemAsync(string id);
+        Task<T> GetItemAsync(string id);
+        Task<IEnumerable<T>> GetItemsAsync(bool forceRefresh = false);
+    }
+}
